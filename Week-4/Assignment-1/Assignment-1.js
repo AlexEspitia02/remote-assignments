8e79ce41eab6caf2de71cf71b6463dcf2eca9727
AlexEspitia02 <146508965+AlexEspitia02@users.noreply.github.com>
2024-04-23 23:47:17 +0800

    fix: Change 'await' syntax (don't use 'then')

--- Week-4/Assignment-1/Assignment-1.js
+++ Week-4/Assignment-1/Assignment-1.js
@@ -31,6 +31,6 @@ delayedResultPromise(4, 5, 3000).then(console.log);
 //第三題
 async function main() {
 // your code here, you should call delayedResultPromise here and get the result using async/await.
-return await delayedResultPromise(4, 5, 3000).then(console.log);
+    return console.log(await delayedResultPromise(4, 5, 3000));
 }
 main(); // result will be shown in the console after <delayTime> seconds
